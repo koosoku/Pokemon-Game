24cdf13b0013a811797a23dd244eb37a2e37d69f
Kyle <kylekung@hotmail.com>
2016-02-27 12:34:35 -0500

    added 2 pokemon objects and percentage of health bars

---
+++ js/javascript.js
@@ -0,0 +1,64 @@
+var chamander = {
+	name: "Charmander",
+	health: 100,
+	lvl: 12,
+	effect: null,
+	moves:[{
+		name:"Ember",
+		type:"Attack",
+		power:90,
+		accuracy:0.80
+	},
+	{
+		name:"Scratch",
+		type:"Attack",
+		power:10,
+		accuracy:0.90
+	},
+	{
+		name:"Leer",
+		type:"Defense",
+		power:0.20,
+		accuracy:1.0
+	},
+	{
+		name:"Growl",
+		type:"Defense",
+		power:0.65,
+		accuracy:0.80
+	},
+	]
+};
+
+var pikachu = {
+	name: "Pikachu",
+	health: 100,
+	lvl: 12,
+	effect: null,
+	moves:[{
+		name:"Thunder Shock",
+		type:"Attack",
+		power:30,
+		accuracy:0.80
+	},
+	{
+		name:"Thunder Wave",
+		type:"Attack",
+		power:10,
+		accuracy:0.90
+	},
+	{
+		name:"Tail Whip",
+		type:"Defense",
+		power:0.20,
+		accuracy:1.0
+	},
+	{
+		name:"Growl",
+		type:"Defense",
+		power:0.65,
+		accuracy:0.80
+	},
+	]
+
+};
